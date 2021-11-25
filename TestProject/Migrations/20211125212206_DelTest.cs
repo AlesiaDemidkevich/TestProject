0a7c0e51f28680687d36534d5f10b3f7b3c82bf6
alesyademidkevich <alesya.demidkevic@mail.ru>
2021-11-26 01:47:56 +0300

    add one test

---
+++ TestProject/Migrations/20211125212206_DelTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestProject.Migrations
+{
+    public partial class DelTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("dbo.Test", "IdVariant");
+            migrationBuilder.DropTable("dbo.Variant");
+        }
+    }
+}
