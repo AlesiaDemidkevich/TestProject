084b508ddf0d2e2f0c94901fcefee79eaba6cf8b
alesyademidkevich <alesya.demidkevic@mail.ru>
2021-11-26 10:52:10 +0300

    create test

---
+++ TestProject/Migrations/20211126072609_AddTestAndQuestions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestProject.Migrations
+{
+    public partial class AddTestAndQuestions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdQuestion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+               name: "Questions",
+               columns: table => new
+               {
+                   Id = table.Column<int>(type: "int", nullable: false)
+                       .Annotation("SqlServer:Identity", "1, 1"),
+                   Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                   IdQuestionCoefficient = table.Column<int>(type: "int", nullable: false),
+                   ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                   IdSubject = table.Column<int>(type: "int", nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Questions", x => x.Id);
+               });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ TestProject/ViewModels/TestViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject.ViewModels
+{
+    public class TestViewModel
+    {
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Question")]
+        public string Question { get; set; }
+
+    }
+}
